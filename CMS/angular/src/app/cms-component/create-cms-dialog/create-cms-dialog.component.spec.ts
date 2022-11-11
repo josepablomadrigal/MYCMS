879dca8d6972bf840d530924a1ff48e8904754af
Jose Pablo <pablomadrigaless@gmail.com>
2022-11-10 21:52:19 -0600

    Included services to manage the modal and call the http request to save the cms

--- CMS/angular/src/app/cms-component/create-cms-dialog/create-cms-dialog.component.spec.ts
+++ CMS/angular/src/app/cms-component/create-cms-dialog/create-cms-dialog.component.spec.ts
@@ -1,25 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateCmsDialogComponent } from './create-cms-dialog.component';
+import { CmsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { HttpClientTestingModule, HttpTestingController } from '@node_modules/@angular/common/http/testing';
+import { BsModalRef } from '@node_modules/ngx-bootstrap/modal';
+import { AppSessionService } from '@shared/session/app-session.service';
 
 describe('CreateCmsDialogComponent', () => {
-  let component: CreateCmsDialogComponent;
-  let fixture: ComponentFixture<CreateCmsDialogComponent>;
+    let component: CreateCmsDialogComponent;
+    let fixture: ComponentFixture<CreateCmsDialogComponent>;
+    let httpController: HttpTestingController;
+    let cmsService: CmsServiceProxy;
+    let bsModalSpy: jasmine.SpyObj<BsModalRef>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreateCmsDialogComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        const bsModalServiceSpy = jasmine.createSpyObj('BsModalService', ['hide']);
+        await TestBed.configureTestingModule({
+            declarations: [CreateCmsDialogComponent],
+            imports: [HttpClientTestingModule],
+            providers: [
+                CmsServiceProxy,
+                {
+                    provide: AppSessionService,
+                    useValue: {
+                        application: {
+                            version: '',
+                            releaseDate: {
+                                format: function () {
+                                    return '';
+                                },
+                            },
+                        },
+                        getShownLoginName: function () {
+                            return 'admin';
+                        }
+                    },
+                },
+                {provide: BsModalRef, useValue: bsModalServiceSpy}
+            ],
+        }).compileComponents();
+        bsModalSpy = TestBed.inject(BsModalRef) as jasmine.SpyObj<BsModalRef>;
+        cmsService = TestBed.inject(CmsServiceProxy);
+        httpController = TestBed.inject(HttpTestingController);
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreateCmsDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CreateCmsDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- CMS/angular/src/app/cms-component/create-cms-dialog/create-cms-dialog.component.ts
+++ CMS/angular/src/app/cms-component/create-cms-dialog/create-cms-dialog.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { EventEmitter, Injector, Output } from '@node_modules/@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { CmsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalRef } from '@node_modules/ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-create-cms-dialog',
-  templateUrl: './create-cms-dialog.component.html',
-  styleUrls: ['./create-cms-dialog.component.css']
+    selector: 'app-create-cms-dialog',
+    templateUrl: './create-cms-dialog.component.html',
+    styleUrls: ['./create-cms-dialog.component.css']
 })
-export class CreateCmsDialogComponent implements OnInit {
+export class CreateCmsDialogComponent extends AppComponentBase implements OnInit {
 
-  constructor() { }
+    @Output() onSave = new EventEmitter<any>();
 
-  ngOnInit(): void {
-  }
+    constructor(injector: Injector,
+                private _cmsServiceProxy: CmsServiceProxy,
+                public bsModalRef: BsModalRef) {
+        super(injector);
+    }
+
+
+    ngOnInit(): void {
+    }
 
 }
